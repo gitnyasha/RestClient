ddaa3d979ba6ca45e7d860b231b5eb24767c2e1c
gitnyasha <marshall.chikari@gmail.com>
2020-03-17 16:53:47 +0200

    added the users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,22 @@
 class UsersController < ApplicationController
   def index
-
   end
+
   def new
-      
   end
+
   def create
-      
   end
+
   def update
-      
   end
+
   def show
-
   end
+
   def edit
-
   end
+
   def destroy
-  
   end
 end
